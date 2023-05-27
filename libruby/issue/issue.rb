48fe77077f23b3e6646b29b1ac88713ff854d9ce
thx <elliot10156@gmail.com>
2023-05-27 22:18:08 +0800

    Remove Ruby module

--- libruby/issue/issue.rb
+++
@@ -1,6 +0,0 @@
-class IssueReporter
-    @@reporter = RBIssue.new
-    def self.add_issue(issue)
-        @@reporter.push_issue(issue)
-    end
-end
--- libruby/utils/similary.rb
+++
@@ -1,6 +0,0 @@
-class Similary 
-    @@matcher = RBSimilary.new
-    def self.match(s1, s2)
-        @@matcher.sim_calculate(s1,s2)
-    end
-end
